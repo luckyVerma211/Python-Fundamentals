1c9570e667458e6fb6180082dc7e400b3b51d5f5
Lucky Verma <109918194+luckyVerma211@users.noreply.github.com>
2022-07-28 23:23:31 +0530

    Add files via upload

---
+++ Armstrong_number.py
@@ -0,0 +1,15 @@
+from ast import Num
+from re import A
+
+
+n=int(input("Enter the number:"))
+s=0
+a=n
+while n>0:
+    d=n%10
+    s=s+d**3
+    n=n//10
+if s==a:
+    print(a,"is a Armstrong Number!!")
+else:
+    print(a,"is not a Armstrong Number!!")
